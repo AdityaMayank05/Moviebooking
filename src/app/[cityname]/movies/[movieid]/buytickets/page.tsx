0fecb3dd0d23b24cc6f1b07292be362d21711543
AdityaMayank05 <108828982+AdityaMayank05@users.noreply.github.com>
2025-03-03 19:55:08 +0530

    done

---
+++ src/app/[cityname]/movies/[movieid]/buytickets/page.tsx
@@ -0,0 +1,16 @@
+"use client"
+import React from 'react'
+import DatePicker from "react-horizontal-datepicker";
+import './BuyTicketsPage.css'
+import Link from 'next/link';
+import { usePathname, useParams } from 'next/navigation'
+
+const page = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default page
